3f533e2342222c6b66554583adec2112828847f9
thilaks201 <r.thilakshan01@gmail.com>
2026-09-24 01:24:16 +0000

    Complete Task 7 PCA analysis

--- repo/scripts/07_pca.R
+++ repo/scripts/07_pca.R
@@ -5,6 +5,7 @@ source("scripts/00_setup.R")
 num_df <- df[, c("loan_amount","rate_of_interest","Interest_rate_spread",
                   "Upfront_charges","term","property_value","income",
                   "Credit_Score","LTV","dtir1")]
+
 num_df <- na.omit(num_df)
 
 pca_result <- prcomp(num_df, scale. = TRUE)
@@ -12,3 +13,7 @@ summary(pca_result)
 
 # TODO: scree plot, discuss whether PCA is worth using given
 # known multicollinearity (loan_amount~property_value etc.)
+
+
+
+plot(pca_result, type = "l")
